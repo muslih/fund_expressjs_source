9a5aded0744032ccf4e9abd7e9cc71a6081face0
Muslih <muslihzarth@gmail.com>
2015-04-05 12:11:08 +0800

    Add some routes

--- 03_verb_methods/server.js
+++ 03_verb_methods/server.js
@@ -1,8 +1,33 @@
 var express = require('express'),
+    bodyParser = require('body-parser'),
     app     = express();
+
+function log(req, res, next){
+	console.log(names);
+	next();
+}
+
+app.use(bodyParser.urlencoded());
 
 var names = [];
 
-app.get('/',function(req, res){
+app.all('/',function(req,res, next){
+	// res.send('Testing all');
+	console.log('from All');
+	next();
+});
+
+app.get('/',log,function(req, res){
 	res.render('index.jade',{names: names});
 });
+
+app.post('/', function(req, res){
+	names.push(req.body.name);
+	res.redirect('/')
+});
+
+// app.put
+
+// app.delete
+
+app.listen(3000);
